7d5b8d225ed9a5822772038a6d04a07c43ce1dc7
FormindVER <135024344+FormindVER@users.noreply.github.com>
2026-03-14 15:55:48 +0100

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "SubDomainizer";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      python = pkgs.python3;
+      py = python.withPackages (
+        ps: with ps; [
+          termcolor
+          beautifulsoup4
+          requests
+          htmlmin
+          tldextract
+          colorama
+          cffi
+        ]
+      );
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation rec {
+        pname = "subdomainizer";
+        version = "unstable-2026-03-14";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "nsonaniya2010";
+          repo = "SubDomainizer";
+          rev = "master";
+          hash = "sha256-DeTk76XfpkklXMsE7QcSf1QsNC4c51GdxZxhp4MJUn8=";
+        };
+
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/${pname} $out/bin
+          cp -r . $out/share/${pname}
+
+          makeWrapper ${py}/bin/python3 $out/bin/SubDomainizer.py \
+            --add-flags "$out/share/${pname}/SubDomainizer.py"
+
+          runHook postInstall
+        '';
+
+        meta = with pkgs.lib; {
+          description = "Find hidden subdomains and secrets in webpages, JS files, folders, and GitHub";
+          homepage = "https://github.com/nsonaniya2010/SubDomainizer";
+          license = licenses.mit;
+          mainProgram = "SubDomainizer.py";
+          platforms = platforms.all;
+        };
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/SubDomainizer.py";
+      };
+    };
+}
